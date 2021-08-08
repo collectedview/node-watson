4a3869066e7e0aefca59b4d0c01ab1f1137ab1d8
Joe Alongi <collectedview@gmail.com>
2021-08-07 20:19:57 -0400

    Production Deployment Updates

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+const watsonRoutes = require("./watson");
+
 require("dotenv").config();
 
 app.use(express.json());
-
-const watsonRoutes = require("./watson");
 
 app.use("/", watsonRoutes);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/*", (request, response) => {
+  response.sendFile(
+    path.join(__dirname, "react-watson", "build", "index.html")
+  );
 });
 
 const port = process.env.PORT || 3030;
 app.listen(port, () => {
-  console.log(`Node Watson now running at http://localhost:${port}`);
+  console.log(`Watson now running at http://localhost:${port}`);
 });
